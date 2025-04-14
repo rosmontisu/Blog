3a7bc0dcfcfb27adca5e206026b139c17f3ba9fc
hepari <107000683+rosmontisu@users.noreply.github.com>
2025-04-14 11:52:45 +0900

    Fix: 도메인 관련 설정 - 서브 도메인으로 CNAME레코드 추가 - base 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: "https://rosmontisu.github.io",
-  base: "/Blog",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(
